b54ae9c5eb6dde6dbb80b95e36f1bfbb5569e76e
pmcriver <156025519+pmcriver@users.noreply.github.com>
2024-01-21 12:02:31 +0000

    Adicionar comentários

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -10,11 +10,22 @@ namespace Calculos
 /// Implementa operações aritméticas
 /// </summary>
     internal class Aritmetica
-    {
+    {/// <summary>
+    /// Operação Somar
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
         public int Somar(int x,int y) 
         { 
             return x + y; 
         }
+        /// <summary>
+        /// Operação Subtrair
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         public int Subtrair(int x, int y)
         {
             return x - y;
